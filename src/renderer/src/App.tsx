72a889565587cf2abaf1f71cc6c2055ce5c379ca
deewakar <dakshftw01@gmail.com>
2024-11-21 00:46:12 +0530

    feat: text editor

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,12 @@
-import { ActionsButtonRow, Content, NotePreviewList, RootLayout, Sidebar } from '@/components'
+import {
+  ActionsButtonRow,
+  Content,
+  Editor,
+  FloatingNoteTitle,
+  NotePreviewList,
+  RootLayout,
+  Sidebar
+} from '@/components'
 
 const App = () => {
   return (
@@ -8,7 +16,10 @@ const App = () => {
           <ActionsButtonRow className="flex justify-between mt-1" />
           <NotePreviewList className="mt-1" />
         </Sidebar>
-        <Content className="border-l bg-zinc-900 border-l-white/20">Content</Content>
+        <Content className="border-l bg-zinc-900 border-l-white/20">
+          <FloatingNoteTitle className="pt-2" />
+          <Editor />
+        </Content>
       </RootLayout>
     </>
   )
